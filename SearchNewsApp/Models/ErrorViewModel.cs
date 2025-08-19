e0f6e77b33e64b9cb3c84fdb25ca6ce7cf2cf50a
Usha Kolla <227265983+ushakolla100-arch@users.noreply.github.com>
2025-08-19 08:20:39 +0530

    reverting
    reverting

--- SearchNewsApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SearchNewsApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
